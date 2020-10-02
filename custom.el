d2daa120f824a4bf8b1555526c361603fb1a7876
Nathan Lovato <nathan@gdquest.com>
2020-10-02 07:36:37 -0600

    Add custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-tcp-connection-timeout 0.5)
+ '(org-agenda-files
+   '("~/Documents/org/jobs/godot-docs-job-1.org" "~/Documents/org/2d-secrets/jrpg-combat.org" "/home/gdquest/Documents/org/calendar.org" "/home/gdquest/Documents/org/tasks.org" "/home/gdquest/Documents/org/ideas.org" "/home/gdquest/Documents/org/notes.org" "/home/gdquest/Documents/org/todo.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
